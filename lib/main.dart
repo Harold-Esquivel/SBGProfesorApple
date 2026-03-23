c2c2bd317d8284c69ed3ab789c244b6ec20e4cd9
JalfMVZ <velascojosealfredo1@gmail.com>
2026-03-23 18:49:11 -0500

    pne

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'splash_screen.dart'; // 👈 ESTE ES EL IMPORT
+import 'splash_screen.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-
 
 const kPrimary = Color.fromARGB(255, 71, 76, 223);
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await initializeDateFormatting('es_ES', null);
   runApp(const MyApp());
 }
@@ -23,10 +20,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SBG Profesores',
-      home: SplashScreen(), 
-    );
+    return MaterialApp(title: 'SBG Profesores', home: SplashScreen());
   }
 }
 
@@ -35,11 +29,7 @@ class PushService {
 
   static Future<void> init() async {
     // iOS: pedir permiso (en Android también sirve)
-    await _fcm.requestPermission(
-      alert: true,
-      badge: true,
-      sound: true,
-    );
+    await _fcm.requestPermission(alert: true, badge: true, sound: true);
 
     // Token del dispositivo (guárdalo en Firestore si quieres)
     final token = await _fcm.getToken();
